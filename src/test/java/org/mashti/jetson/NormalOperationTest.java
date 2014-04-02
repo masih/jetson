07988f4bd7f3287e8128086dc23a48a03d5e1f4c
masih <masiih@gmail.com>
2014-04-02 18:31:30 +0100

    investigating test failure on travis ci

--- src/test/java/org/mashti/jetson/NormalOperationTest.java
+++ src/test/java/org/mashti/jetson/NormalOperationTest.java
@@ -217,11 +217,11 @@ public class NormalOperationTest extends AbstractTest {
     @Test
     public void testConcurrentServers() throws RPCException, InterruptedException, ExecutionException {
 
-        final ExecutorService executor = Executors.newFixedThreadPool(500);
+        final ExecutorService executor = Executors.newFixedThreadPool(100);
         try {
             final CountDownLatch start_latch = new CountDownLatch(1);
             final List<Future<Void>> future_concurrent_tests = new ArrayList<Future<Void>>();
-            for (int i = 0; i < 500; i++) {
+            for (int i = 0; i < 100; i++) {
                 future_concurrent_tests.add(executor.submit(new Callable<Void>() {
 
                     @Override
